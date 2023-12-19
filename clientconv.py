79e839231bada5ad9afb87238d414e32f98e0dc2
SaiGaneshEswaraprasad <113225851+SaiGaneshEswaraprasad@users.noreply.github.com>
2023-12-19 06:23:57 +0000

    done

--- clientconv.py
+++ clientconv.py
@@ -5,15 +5,10 @@ port = 5004
 s.connect((socket.gethostname(), port))
 
 while True:
-    # Client receives a message
     data = s.recv(1024)
     if not data:
         break
     print(f"Server: {data.decode()}")
-
-    # Client sends a message
     message_to_send = input("Client: ")
     s.send(bytes(message_to_send, "utf-8"))
-
-# Close the connection
 s.close()
--- serverconv.py
+++ serverconv.py
@@ -3,23 +3,19 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 5004
 s.bind((socket.gethostname(), port))
-s.listen(1)  # Allow only one connection
+s.listen(10)
 
 print("Waiting for a connection...")
 c, adr = s.accept()
 print(f"Connected to {adr} established")
 
 while True:
-    # Server sends a message
     message_to_send = input("Server: ")
     c.send(bytes(message_to_send, "utf-8"))
-
-    # Server receives a message
     data = c.recv(1024)
     if not data:
         break
     print(f"Client: {data.decode()}")
 
-# Close the connection
 print(f"Connection with {adr} closed.")
 c.close()
