1b1eed91cd2c068c18cc038f62fbac154a46677f
SaiGaneshEswaraprasad <113225851+SaiGaneshEswaraprasad@users.noreply.github.com>
2023-12-19 10:57:14 +0530

    done

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port=5003
+s.connect(socket.gethostname(),port)
+print("Connected")
+z="hello from client"
+z=str.encode(z,"utf-8")
+s.send(z)
+print(z.decode("utf-8"))
