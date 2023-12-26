d24f530564651c3525d80ef1192804a2153b294a
SaiGaneshEswaraprasad <113225851+SaiGaneshEswaraprasad@users.noreply.github.com>
2023-12-26 05:26:38 +0000

    done

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-port=5004
-s.connect((socket.gethostname(),port))
-print("Connected")
-z="hello from client"
-z=str.encode(z,"utf-8")
-s.send(z)
-print(z.decode("utf-8"))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 5004
+client.connect((socket.gethostname(), port))
+print(f"Connected to server at {socket.gethostname()}:{port}")
+
+message_to_server = "Hello from the client!"
+client.send(bytes(message_to_server, 'utf-8'))
+
+data = client.recv(1024)
+print("Received from server:", data.decode())
+
+client.close()
